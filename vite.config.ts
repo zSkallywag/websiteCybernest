75f6cc4b8a334256f851480d28eb8896404a1a04
Gaetano Cesano <56698911+zSkallywag@users.noreply.github.com>
2025-06-23 12:42:28 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cybernest-website/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
